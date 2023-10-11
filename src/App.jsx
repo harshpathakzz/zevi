b71a30b1c21950c61321900d3f53efc9285eaba4
harshpathakzz <harshpathak2017@gmail.com>
2023-10-11 14:16:54 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import SearchBar from "./components/SearchBar";
+import ProductCard from "./components/ProductCard";
 function App() {
   return (
     <>
       <div>
         <SearchBar />
+        <ProductCard />
       </div>
     </>
   );
