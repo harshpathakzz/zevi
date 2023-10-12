9f3c51a3b1893df1342843f03361f98bd4f29a21
harshpathakzz <harshpathak2017@gmail.com>
2023-10-12 13:33:16 +0530

    updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import "./SearchBar.scss";
 import SearchIcon from "@mui/icons-material/Search";
 import { FilterContext } from "../context/FilterContext";
@@ -6,12 +6,36 @@ import { FilterContext } from "../context/FilterContext";
 const SearchBar = () => {
   const { dispatch } = useContext(FilterContext);
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const searchContainerRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        searchContainerRef.current &&
+        !searchContainerRef.current.contains(event.target)
+      ) {
+        setShowSuggestions(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const handleSearchChange = (searchQuery) => {
     dispatch({
       type: "UPDATE_SEARCH_QUERY",
       searchQuery: searchQuery,
     });
+    setShowSuggestions(false);
+    setSearchValue(searchQuery);
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setSearchValue(suggestion);
     setShowSuggestions(false);
   };
 
@@ -20,12 +44,13 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="search-bar">
+    <div className="search-bar" ref={searchContainerRef}>
       <input
         type="text"
         placeholder="Search"
         onClick={handleInputClick}
         onChange={(e) => handleSearchChange(e.target.value)}
+        value={searchValue}
       />
       <SearchIcon />
       {showSuggestions && (
@@ -36,8 +61,12 @@ const SearchBar = () => {
             <p>Product Title</p>
           </div>
           <h3>Popular Suggestions</h3>
-          <p>Popular Suggestion 1</p>
-          <p>Popular Suggestion 2</p>
+          <p onClick={() => handleSuggestionClick("Popular Suggestion 1")}>
+            Popular Suggestion 1
+          </p>
+          <p onClick={() => handleSuggestionClick("Popular Suggestion 2")}>
+            Popular Suggestion 2
+          </p>
         </div>
       )}
     </div>
