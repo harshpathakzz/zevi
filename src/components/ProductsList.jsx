294a7c691351c20796de98ca5b870eeba3e17bbd
harshpathakzz <harshpathak2017@gmail.com>
2023-10-12 12:09:43 +0530

    updated

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,6 +1,6 @@
 // ProductsList.js
 
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { FilterContext } from "../context/FilterContext";
 
 const ProductsList = () => {
