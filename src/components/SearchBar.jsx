5c5a25da0f46461a76e478e88ca855fed806158b
harshpathakzz <harshpathak2017@gmail.com>
2023-10-12 13:02:40 +0530

    updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./SearchBar.scss";
 import SearchIcon from "@mui/icons-material/Search";
-const SearchBar = ({ onSearchChange }) => {
+import { FilterContext } from "../context/FilterContext";
+
+const SearchBar = () => {
+  const { dispatch } = useContext(FilterContext);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const handleSearchChange = (searchQuery) => {
+    dispatch({
+      type: "UPDATE_SEARCH_QUERY",
+      searchQuery: searchQuery,
+    });
+    setShowSuggestions(false);
+  };
+
+  const handleInputClick = () => {
+    setShowSuggestions(true);
+  };
+
   return (
     <div className="search-bar">
       <input
         type="text"
         placeholder="Search"
-        onChange={(e) => onSearchChange(e.target.value)}
+        onClick={handleInputClick}
+        onChange={(e) => handleSearchChange(e.target.value)}
       />
       <SearchIcon />
+      {showSuggestions && (
+        <div className="suggestions-container">
+          <h3>Latest Trends</h3>
+          <div className="product-trend">
+            <img src="product-image-url" alt="Product" />
+            <p>Product Title</p>
+          </div>
+          <h3>Popular Suggestions</h3>
+          <p>Popular Suggestion 1</p>
+          <p>Popular Suggestion 2</p>
+        </div>
+      )}
     </div>
   );
 };
